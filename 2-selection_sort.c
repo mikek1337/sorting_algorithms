c10354d86e393abb5a425f10a2d89a0933f065be
mikek1337 <mikekegnlyew@gmail.com>
2022-11-07 22:05:55 +0300

    selection sort ;

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	 size_t i, j;
+	 int currMin;
+	 int temp;
+	for (i = 0; i < size; i++)
+	{
+		currMin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[currMin])
+			{
+				currMin = j;
+			}
+
+		}
+		temp = array[i];
+		array[i] = array[currMin];
+		array[currMin] = temp;
+		print_array(array, size);
+	}
+}
