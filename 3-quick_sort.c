fce7b0c3fe7c0eb904ab52cab3b28834a461c09c
mikek1337 <mikekegnlyew@gmail.com>
2022-11-08 06:44:00 -0500

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * quick_sort - quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
+
+int partition(int *array, int low, int high)
+{
+	
+}
